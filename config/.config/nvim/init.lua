581b6dc713ef16916a7c023f2c0bfedcd147d7fd
agaro <anthony.garo@disneystreaming.com>
2023-04-17 14:19:34 -0400

    removes unused code

--- config/.config/nvim/init.lua
+++ config/.config/nvim/init.lua
@@ -18,22 +18,4 @@ require("lazy").setup("user.plugins")
 require("user.colorscheme")
 require("user.autocmd")
 
---require "user.plugins"
---require "user.theme"
---require "user.nvimtree"
---require "user.airline"
---require "user.cmp"
--- require "user.coc"
---require "user.metals"
---require "user.indent-blankline"
---require "user.lsp-config"
---require "user.treesitter"
---require "user.autocmd"
---require "user.symbols-outline"
---require "user.smart-splits"
---require "user.peek"
---require "user.nvim-surround"
---require "user.nvim-treesitter-textobjects"
---require "user.autoclose"
-
 vim.cmd('source ~/.config/nvim/vimrc.vim')
