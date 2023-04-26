240d15b820860aec7f38bfbf6b48d984ba835883
agaro <anthony.garo@disneystreaming.com>
2023-04-26 11:16:09 -0400

    more themes

--- config/.config/nvim/lua/user/colorscheme.lua
+++ config/.config/nvim/lua/user/colorscheme.lua
@@ -1,1 +1,1 @@
-vim.cmd([[ colorscheme palenight ]])
+vim.cmd("colorscheme palenight")
--- config/.config/nvim/lua/user/plugins/colorscheme.lua
+++ config/.config/nvim/lua/user/plugins/colorscheme.lua
@@ -7,7 +7,30 @@ return {
   {
     "drewtempelmeyer/palenight.vim",
     lazy = true,
+  },
+  {
+    "sainnhe/sonokai",
+    lazy = true,
+    config = function()
+      vim.g.sonokai_style = 'default'
+    end
+  },
+  {
+    "tanvirtin/monokai.nvim",
+    lazy = true
+  },
+  {
+    "lourenci/github-colors",
+    lazyu = true
+  },
+  {
+    "olimorris/onedarkpro.nvim",
+    lazy = true
+  },
+  {
+    "tiagovla/tokyodark.nvim",
+    lazy = true
   }
 
-}
 
+}
