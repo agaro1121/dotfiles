3e03de0e3a3b91063a11aa98a7d6330bb25be561
Anthony Garo <agaro@Anthonys-MacBook-Pro.local>
2023-04-25 21:01:08 -0400

    adds telescope select

--- config/.config/nvim/lua/user/plugins/telescope.lua
+++ config/.config/nvim/lua/user/plugins/telescope.lua
@@ -1,5 +1,19 @@
 return {
-    'nvim-telescope/telescope.nvim', tag = '0.1.1',
+  {
+    "nvim-telescope/telescope-ui-select.nvim",
+    config = function()
+      require("telescope").setup {
+        extensions = {
+          ["ui-select"] = {
+            require("telescope.themes").get_dropdown {}
+          }
+        }
+      }
+    end
+  },
+  {
+    'nvim-telescope/telescope.nvim',
+    tag = '0.1.1',
     keys = {
       { "<leader>ff", "<cmd>lua require('telescope.builtin').find_files()<cr>" },
       { "<leader>lg", "<cmd>lua require('telescope.builtin').live_grep()<cr>" },
@@ -17,7 +31,8 @@ return {
         end
         require("telescope.builtin").lsp_workspace_symbols({ query = input })
       end
-        require("telescope").setup()
-    return { M }
+      require("telescope").load_extension("ui-select")
+      return { M }
     end,
+  }
 }
