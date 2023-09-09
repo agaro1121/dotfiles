6c3f0a98b103d5548f5fd01be431fd6c1768bcc5
Anthony Garo <agaro@Anthonys-MacBook-Pro.local>
2023-09-09 15:32:02 -0400

    diffview, disable nvim tree on open

--- config/.config/nvim/lua/user/autocmd.lua
+++ config/.config/nvim/lua/user/autocmd.lua
@@ -1,19 +1,19 @@
-local function open_nvim_tree(data)
-  -- buffer is a directory
-  local directory = vim.fn.isdirectory(data.file) == 1
+-- local function open_nvim_tree(data)
+--   -- buffer is a directory
+--   local directory = vim.fn.isdirectory(data.file) == 1
+--
+--   if not directory then
+--     return
+--   end
+--
+--   -- change to the directory
+--   vim.cmd.cd(data.file)
+--
+--   -- open the tree
+--   require("nvim-tree.api").tree.open({ focus = false, find_file = true, })
+-- end
 
-  if not directory then
-    return
-  end
-
-  -- change to the directory
-  vim.cmd.cd(data.file)
-
-  -- open the tree
-  require("nvim-tree.api").tree.open({ focus = false, find_file = true, })
-end
-
-vim.api.nvim_create_autocmd({ "VimEnter" }, { callback = open_nvim_tree })
+-- vim.api.nvim_create_autocmd({ "VimEnter" }, { callback = open_nvim_tree })
 
 -- breadcrumb auto attach
 vim.api.nvim_create_autocmd("LspAttach", {
