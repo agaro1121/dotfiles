01b0bc90e5aabad4fc565bc62fbd089b9e1df263
Anthony Garo <agaro@Anthonys-MacBook-Pro.local>
2023-09-09 15:05:05 -0400

    open nvimtree on startup, set highlight group for diff

--- config/.config/nvim/lua/user/autocmd.lua
+++ config/.config/nvim/lua/user/autocmd.lua
@@ -1,19 +1,19 @@
--- local function open_nvim_tree(data)
---   -- buffer is a directory
---   local directory = vim.fn.isdirectory(data.file) == 1
---
---   if not directory then
---     return
---   end
---
---   -- change to the directory
---   vim.cmd.cd(data.file)
---
---   -- open the tree
---   require("nvim-tree.api").tree.open({ focus = false, find_file = true, })
--- end
+local function open_nvim_tree(data)
+  -- buffer is a directory
+  local directory = vim.fn.isdirectory(data.file) == 1
 
--- vim.api.nvim_create_autocmd({ "VimEnter" }, { callback = open_nvim_tree })
+  if not directory then
+    return
+  end
+
+  -- change to the directory
+  vim.cmd.cd(data.file)
+
+  -- open the tree
+  require("nvim-tree.api").tree.open({ focus = false, find_file = true, })
+end
+
+vim.api.nvim_create_autocmd({ "VimEnter" }, { callback = open_nvim_tree })
 
 -- breadcrumb auto attach
 vim.api.nvim_create_autocmd("LspAttach", {
