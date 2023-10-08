dd0b01f5253649dcb6026df5e802245efba5b97a
agaro1121 <agaro1121@gmail.com>
2023-10-08 08:35:19 -0400

    updates git blame default message

--- config/.config/nvim/lua/user/plugins/git-blame.lua
+++ config/.config/nvim/lua/user/plugins/git-blame.lua
@@ -2,6 +2,7 @@ return {
   "f-person/git-blame.nvim",
   config = function()
     vim.g.gitblame_display_virtual_text = 1 -- hides virtual text. Moved it to lualine.
+    vim.g.gitblame_message_when_not_committed = 'GitBlame: Not Committed Yet'
     vim.cmd("GitBlameToggle") -- this is the only way?
     vim.keymap.set("n", "<leader>bl", ":GitBlameToggle<CR>")
   end
