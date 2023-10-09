c175c0b2700abba80993e97f1c135946a97e1a5c
agaro1121 <agaro1121@gmail.com>
2023-10-09 15:34:03 -0400

    adds function to dump tables

--- config/.config/nvim/init.lua
+++ config/.config/nvim/init.lua
@@ -17,3 +17,24 @@ require("lazy").setup("user.plugins")
 require("user.colorscheme")
 
 vim.cmd('source ~/.config/nvim/vimrc.vim')
+
+function dump(t, indent, done)
+    done = done or {}
+    indent = indent or 0
+
+    done[t] = true
+
+    for key, value in pairs(t) do
+        print(string.rep("\t", indent))
+
+        if type(value) == "table" and not done[value] then
+            done[value] = true
+            print(key, ":\n")
+
+            dump(value, indent + 2, done)
+            done[value] = nil
+        else
+            print(key, "\t=\t", value, "\n")
+        end
+    end
+end
