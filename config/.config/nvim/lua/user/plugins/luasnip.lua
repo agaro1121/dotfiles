db861be66b645a2ca0189b4b957f413f5ddf4493
agaro1121 <agaro1121@gmail.com>
2023-10-09 22:08:17 -0400

    uses luasnip

---
+++ config/.config/nvim/lua/user/plugins/luasnip.lua
@@ -0,0 +1,22 @@
+return {
+  "L3MON4D3/LuaSnip",
+  dependencies = { "saadparwaiz1/cmp_luasnip" },
+  -- follow latest release.
+  version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
+  -- install jsregexp (optional!).
+  build = "make install_jsregexp",
+  config = function()
+    local ls = require("luasnip")
+
+    map("i",       "<C-K>", function() ls.expand() end)
+    map({"i","s"}, "<C-L>", function() ls.jump(1)  end)
+    map({"i","s"}, "<C-H>", function() ls.jump(-1) end)
+
+    map({"i","s"}, "<C-E>", function()
+      if ls.choice_active() then
+        ls.change_choice(1)
+      end
+    end)
+  end
+}
+
