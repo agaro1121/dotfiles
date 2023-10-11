ec052b511cc6c8bcff13cdc00acd7bc43c8be5c1
agaro <anthony.garo@disneystreaming.com>
2023-10-11 11:57:58 -0400

    adds dap python, removes breadcrumb from lock

---
+++ config/.config/nvim/lua/user/plugins/nvim-dap-python.lua
@@ -0,0 +1,49 @@
+return {
+  "mfussenegger/nvim-dap-python",
+  dependencies = { "mfussenegger/nvim-dap" },
+  config = function()
+    local dapPython = require("dap-python")
+
+    dapPython.setup("~/.virtualenvs/debugpy/bin/python")
+    dapPython.test_runner = "pytest"
+    map("n", "<leader>dm", function()
+      require('dap-python').test_method({ config = { justMyCode = false } })
+    end)
+
+    -- NONE OF THIS WORKS!
+
+    -- local dap = require("dap")
+
+    -- table.insert(dap.configurations.python, {
+    --   type = 'python',
+    --   request = 'test',
+    --   justMyCode = false,
+    --   name = 'My custom launch configuration',
+    --   program = '${file}',
+    -- })
+
+    -- local configurations = dap.configurations.python
+    -- for _, configuration in pairs(configurations) do
+    --   configuration.justMyCode = false
+    -- end
+
+    -- dap.configurations.python = {
+    --   {
+    --     name = "Debug Unit Test",
+    --     type = "python",
+    --     request = "test",
+    --     justMyCode = false,
+    --   },
+    --   {
+    --     name = "RunProgram",
+    --     type = "python",
+    --     request = "launch",
+    --     program = "${file}",
+    --     justMyCode = false,
+    --   },
+    -- }
+
+    -- NONE OF THIS WORKS!
+
+  end
+}
