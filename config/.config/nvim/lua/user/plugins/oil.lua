74a9ea06d4a0db077273193a09002d4fbdc1964a
agaro1121 <agaro1121@gmail.com>
2023-10-12 22:02:25 -0400

    adds oil

---
+++ config/.config/nvim/lua/user/plugins/oil.lua
@@ -0,0 +1,10 @@
+return {
+  "stevearc/oil.nvim",
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  config = function ()
+    require("oil").setup({
+      default_file_explorer = false,
+    })
+    map("n", "-", ":Oil<CR>")
+  end
+}
