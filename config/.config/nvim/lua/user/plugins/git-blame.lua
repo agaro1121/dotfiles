774b079dff0028a4d86c93ad888a04747c9f5998
agaro <anthony.garo@disneystreaming.com>
2023-10-17 15:49:19 -0400

    disables gitblame again

--- config/.config/nvim/lua/user/plugins/git-blame.lua
+++ config/.config/nvim/lua/user/plugins/git-blame.lua
@@ -3,7 +3,8 @@ return {
   config = function()
     vim.g.gitblame_display_virtual_text = 0 -- hides virtual text. Moved it to lualine.
     vim.g.gitblame_message_when_not_committed = 'GitBlame: Not Committed Yet'
-    vim.g.gitblame_enabled = 0 -- disable on start. Huge perf hit when operating on many files at once.
+    vim.g.gitblame_enabled = 0              -- disable on start. Huge perf hit when operating on many files at once.
     vim.keymap.set("n", "<leader>bl", ":GitBlameToggle<CR>")
+    vim.cmd([[:GitBlameDisable]])
   end
 }
