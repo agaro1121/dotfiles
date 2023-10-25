e67065f5d734cc72756f67d43dd0bf376f3851b5
agaro1121 <agaro1121@gmail.com>
2023-10-25 17:17:12 -0400

    updates, telescope fzf

--- config/.config/nvim/lua/user/plugins/telescope.lua
+++ config/.config/nvim/lua/user/plugins/telescope.lua
@@ -1,55 +1,58 @@
 return {
-  {
+  "nvim-telescope/telescope.nvim",
+  keys = {
+    { "<leader>ff", "<cmd>lua require('telescope.builtin').find_files({hidden=true})<cr>" },
+    { "<leader>fg", "<cmd>lua require('telescope.builtin').git_files({show_untracked=true})<cr>" },
+    { "<leader>fo", "<cmd>lua require('telescope.builtin').oldfiles({only_cwd=true})<cr>" },
+    { "<leader>lg", "<cmd>lua require('telescope.builtin').live_grep()<cr>" },
+    { "<leader>bs",
+      "<cmd>lua require('telescope.builtin').buffers({show_all_buffers = false, sort_mru=true, ignore_current_buffer=false})<cr>" },
+    { "<leader>sp", "<cmd>lua require('telescope.builtin').spell_suggest()<cr>" },
+    { "<leader>fb", "<cmd>lua require('telescope.builtin').current_buffer_fuzzy_find()<cr>" },
+    { "<leader>gs", "<cmd>lua require('telescope.builtin').grep_string()<cr>" },
+  },
+  dependencies = {
+    "nvim-lua/plenary.nvim",
     "nvim-telescope/telescope-ui-select.nvim",
-    config = function()
-      require("telescope").setup {
-        extensions = {
-          ["ui-select"] = {
-            require("telescope.themes").get_dropdown {}
-          }
+    { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+    "nvim-tree/nvim-web-devicons",
+  },
+  config = function()
+    local telescope = require("telescope")
+
+    telescope.setup({
+      extensions = {
+        ["ui-select"] = {
+          require("telescope.themes").get_dropdown {}
+        },
+        fzf = {
+          fuzzy = true,                   -- false will only do exact matching
+          override_generic_sorter = true, -- override the generic sorter
+          override_file_sorter = true,    -- override the file sorter
+          case_mode = "smart_case",       -- or "ignore_case" or "respect_case", the default case_mode is "smart_case"
+        }
+      },
+      defaults = {
+        -- args for live_grep and grep_string
+        vimgrep_arguments = { -- install ripgrep!
+          'rg',
+          '--color=never',
+          '--no-heading',
+          '--with-filename',
+          '--line-number',
+          '--column',
+          '--smart-case',
+          '--hidden',
+          -- '--glob',
+          -- '!{**/.git/*, **/.metals/*}', --do not search git,metals directory
+        },
+        file_ignore_patterns = {
+          ".git/.*"
         }
       }
-    end
-  },
-  {
-    'nvim-telescope/telescope.nvim',
-    tag = '0.1.4',
-    keys = {
-      { "<leader>ff", "<cmd>lua require('telescope.builtin').find_files({hidden=true})<cr>" },
-      { "<leader>fg", "<cmd>lua require('telescope.builtin').git_files({show_untracked=true})<cr>" },
-      { "<leader>fo", "<cmd>lua require('telescope.builtin').oldfiles({only_cwd=true})<cr>" },
-      { "<leader>lg", "<cmd>lua require('telescope.builtin').live_grep()<cr>" },
-      { "<leader>bs", "<cmd>lua require('telescope.builtin').buffers({show_all_buffers = false, sort_mru=true, ignore_current_buffer=false})<cr>" },
-      { "<leader>sp", "<cmd>lua require('telescope.builtin').spell_suggest()<cr>" },
-      { "<leader>fb", "<cmd>lua require('telescope.builtin').current_buffer_fuzzy_find()<cr>" },
-      { "<leader>gs", "<cmd>lua require('telescope.builtin').grep_string()<cr>" },
-    },
-    dependencies = { 'nvim-lua/plenary.nvim' },
-    config = function()
-      local telescope = require("telescope")
+    })
 
-      telescope.setup({
-        defaults = {
-          -- args for live_grep and grep_string
-          vimgrep_arguments = { -- install ripgrep!
-            'rg',
-            '--color=never',
-            '--no-heading',
-            '--with-filename',
-            '--line-number',
-            '--column',
-            '--smart-case',
-            '--hidden',
-            -- '--glob',
-            -- '!{**/.git/*, **/.metals/*}', --do not search git,metals directory
-          },
-          file_ignore_patterns = {
-            ".git/.*"
-          }
-        }
-      })
-
-      telescope.load_extension("ui-select")
-    end
-  }
+    telescope.load_extension("ui-select")
+    telescope.load_extension("fzf")
+  end
 }
