2a0d3f6d40fe603ada20529a9522563de2a240c8
agaro <anthony.garo@disneystreaming.com>
2023-11-10 10:22:17 -0500

    new keybinds for window navigation

--- config/.config/nvim/lua/user/keybinds.lua
+++ config/.config/nvim/lua/user/keybinds.lua
@@ -6,11 +6,11 @@ map("n", "<C-l>", "<C-w>l")
 map("n", "<leader>th", "<C-w>t<C-w>H", {desc="Switches horizontal panes to vertial panes"})
 map("n", "<leader>tk", "<C-w>t<C-w>K", {desc="Switches vertical panes to horizontal panes"})
 
--- break the habit
-map({"n", "i"}, "<Up>",    "<NOP>")
-map({"n", "i"}, "<Down>",  "<NOP>")
-map({"n", "i"}, "<Left>",  "<NOP>")
-map({"n", "i"}, "<Right>", "<NOP>")
+-- window sizing
+map({"n", "i"}, "<Up>",    ":resize -2<CR>")
+map({"n", "i"}, "<Down>",  ":resize +2<CR>")
+map({"n", "i"}, "<Left>",  ":vertical resize +2<CR>")
+map({"n", "i"}, "<Right>", ":vertical resize -2<CR>")
 
 -- better experience
 map({"n", "v"}, "<Space>", "<Nop>")
@@ -19,3 +19,4 @@ map({"n", "v"}, "<Space>", "<Nop>")
 map("v", "J", ":mo '>+1<CR>gv=gv", {desc="Move highlighted lines down"})
 map("v", "K", ":mo '<-2<CR>gv=gv", {desc="Move highlighted lines up"})
 map("x", "<leader>p", "\"_dP", {desc="Paste without having your register overwritten"})
+map("x", "<leader>c", "\"_c", {desc="Paste without having your register overwritten"})
