5938ebcdb8f412dde1879091fff1aa6fc9f1d554
agaro1121 <agaro1121@gmail.com>
2023-11-09 22:55:07 -0500

    updates trouble config

--- config/.config/nvim/lua/user/plugins/trouble.lua
+++ config/.config/nvim/lua/user/plugins/trouble.lua
@@ -1,15 +1,17 @@
 return {
   "folke/trouble.nvim",
   dependencies = { "nvim-tree/nvim-web-devicons" },
-  opts = {
-    -- your configuration comes here
-    -- or leave it empty to use the default settings
-    -- refer to the configuration section below
-    action_keys = {           -- key mappings for actions in the trouble list
-      preview = "o",
-      close_folds = { "zC" }, -- close all folds
-      open_folds = { "zO" },  -- open all folds
-      toggle_fold = { "zo" }, -- toggle fold of current file
-    },
-  },
+  config = function()
+    require("trouble").setup({
+      -- your configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+      action_keys = {         -- key mappings for actions in the trouble list
+        close_folds = { "zC" }, -- close all folds
+        open_folds = { "zO" }, -- open all folds
+        toggle_fold = { "zo" }, -- toggle fold of current file
+      },
+    })
+    map("n", "<leader>tr", require("trouble").toggle)
+  end
 }
