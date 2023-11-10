0d5a6409a0eaefb2eceae62f44f6fa32b0b39764
agaro1121 <agaro1121@gmail.com>
2023-11-09 22:41:11 -0500

    Updates, adds trouble

---
+++ config/.config/nvim/lua/user/plugins/trouble.lua
@@ -0,0 +1,15 @@
+return {
+  "folke/trouble.nvim",
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  opts = {
+    -- your configuration comes here
+    -- or leave it empty to use the default settings
+    -- refer to the configuration section below
+    action_keys = {           -- key mappings for actions in the trouble list
+      preview = "o",
+      close_folds = { "zC" }, -- close all folds
+      open_folds = { "zO" },  -- open all folds
+      toggle_fold = { "zo" }, -- toggle fold of current file
+    },
+  },
+}
