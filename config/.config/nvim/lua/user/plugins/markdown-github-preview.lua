5a34394a32253f38bb1c1808ea6a6f4642e87e77
agaro1121 <agaro1121@gmail.com>
2023-11-11 14:12:23 -0500

    uses different markdown preview

---
+++ config/.config/nvim/lua/user/plugins/markdown-github-preview.lua
@@ -0,0 +1,21 @@
+return {
+  "wallpants/github-preview.nvim",
+  cmd = { "GithubPreviewToggle" },
+  keys = { "<leader>md" },
+  opts = {
+    -- config goes here
+  },
+  config = function(_, opts)
+    local gpreview = require("github-preview")
+    gpreview.setup(opts)
+
+    local fns = gpreview.fns
+    vim.keymap.set("n", "<leader>md", fns.toggle)
+    vim.keymap.set("n", "<leader>mds", fns.single_file_toggle)
+    vim.keymap.set("n", "<leader>mdd", fns.details_tags_toggle)
+  end,
+}
+-- NOTE:
+-- requires Bun
+-- brew tap oven-sh/bun
+-- brew install bun
