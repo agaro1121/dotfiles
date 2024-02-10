d89fe073cb8a99b908a9979bf773277f00e2839b
agaro1121 <agaro1121@gmail.com>
2024-02-10 12:32:12 -0500

    alacritty migration, iterm plist, vim updates

--- config/.config/nvim/lua/user/plugins/lsp_signature.lua
+++ config/.config/nvim/lua/user/plugins/lsp_signature.lua
@@ -2,7 +2,8 @@ return {
   "ray-x/lsp_signature.nvim",
   config = function ()
     require("lsp_signature").setup{
-      hint_enable = false
+      hint_enable = false,
+      auto_close_after = 5,
     }
   end
 }
