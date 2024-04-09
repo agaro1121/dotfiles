f5d36825967f89e9323ff07188cdc3a7f5748687
agaro <anthony.garo@disneystreaming.com>
2024-04-09 10:34:12 -0400

    fmt

--- config/.config/nvim/lua/user/plugins/toggleterm.lua
+++ config/.config/nvim/lua/user/plugins/toggleterm.lua
@@ -31,7 +31,12 @@ return {
 
     -- lazyvim
     local Terminal = require('toggleterm.terminal').Terminal
-    local lazygit  = Terminal:new({ cmd = "lazygit", hidden = true, direction = "float", count = 1000 })
+    local lazygit  = Terminal:new({
+      cmd = "lazygit",
+      hidden = true,
+      direction = "float",
+      count = 99
+    })
 
     function _lazygit_toggle()
       lazygit:toggle()
