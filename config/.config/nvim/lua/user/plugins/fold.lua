239936eea3cd82818ff5f849cd57c0f25ced6672
agaro1121 <agaro1121@gmail.com>
2024-09-26 21:18:26 -0400

    comments out statuscol

--- config/.config/nvim/lua/user/plugins/fold.lua
+++ config/.config/nvim/lua/user/plugins/fold.lua
@@ -3,20 +3,20 @@ return {
     "kevinhwang91/nvim-ufo",
     dependencies = {
       "kevinhwang91/promise-async",
-      {
-        "luukvbaal/statuscol.nvim",
-        config = function()
-          local builtin = require("statuscol.builtin")
-          require("statuscol").setup({
-            relculright = true,
-            segments = {
-              { text = { builtin.foldfunc },      click = "v:lua.ScFa" },
-              { text = { "%s" },                  click = "v:lua.ScSa" },
-              { text = { builtin.lnumfunc, " " }, click = "v:lua.ScLa" },
-            },
-          })
-        end,
-      },
+      -- {
+      --   "luukvbaal/statuscol.nvim",
+      --   config = function()
+      --     local builtin = require("statuscol.builtin")
+      --     require("statuscol").setup({
+      --       relculright = true,
+      --       segments = {
+      --         { text = { builtin.foldfunc },      click = "v:lua.ScFa" },
+      --         { text = { "%s" },                  click = "v:lua.ScSa" },
+      --         { text = { builtin.lnumfunc, " " }, click = "v:lua.ScLa" },
+      --       },
+      --     })
+      --   end,
+      -- },
     },
     config = function()
       vim.o.fillchars = [[eob: ,fold: ,foldopen:,foldsep: ,foldclose:]]
