1efe899eaa96034d6447a90e39467e5c23dbcc33
agaro1121 <agaro1121@gmail.com>
2025-05-11 14:57:28 -0400

    Adds lazydev to cmp. Customizes columns appearance.

--- config/.config/nvim/lua/user/plugins/blink-cmp.lua
+++ config/.config/nvim/lua/user/plugins/blink-cmp.lua
@@ -1,113 +1,99 @@
--- return {
---   {
---     "saghen/blink.cmp",
---     dependencies = { 'rafamadriz/friendly-snippets' },
-
---     -- use a release tag to download pre-built binaries
---     version = '1.*',
---     config = function()
---       require('blink-cmp').setup({
---         keymap = {
---           preset = 'default',
---           ['<CR>'] = { 'select_and_accept', 'fallback' },
---         },
---         sources = {
---           default = {'lsp', 'buffer', 'snippets', 'path' },
---         },
---         completion = {
---           ghost_text = {
---             enabled = true,
---           },
---           documentation = {
---             auto_show = true,
---           }
---         },
---         signature = {
---           enabled = true,
---         },
---         fuzzy = {
---           implementation = "prefer_rust_with_warning",
---         },
---       })
---     end
---   }
--- }
-
 return {
-  'saghen/blink.cmp',
-  dependencies = { 'rafamadriz/friendly-snippets', "onsails/lspkind.nvim", "nvim-tree/nvim-web-devicons" },
-
-  -- use a release tag to download pre-built binaries
-  version = '1.*',
-  opts = {
-    keymap = {
-      preset = 'default',
-      ['<CR>'] = { 'accept', 'fallback' },
-      ['<C-l>'] = { 'snippet_forward', 'fallback' },
-      ['<C-h>'] = { 'snippet_backward', 'fallback' },
-    },
-    appearance = {
-      nerd_font_variant = 'mono'
-    },
-    completion = {
-      documentation = {
-        auto_show = true,
-        auto_show_delay_ms = 500
+  {
+    "folke/lazydev.nvim",
+    ft = "lua", -- only load on lua files
+    opts = {
+      library = {
+        -- See the configuration section for more details
+        -- Load luvit types when the `vim.uv` word is found
+        { path = "${3rd}/luv/library", words = { "vim%.uv" } },
       },
-      accept = {
-        auto_brackets = { -- automatically insert brackets when accepting functions/methods
-          enabled = true,
-          semantic_token_resolution = {
-            blocked_filetypes = { "scala" },
-          }
-        }
+    },
+  },
+  {
+    'saghen/blink.cmp',
+    dependencies = { 'rafamadriz/friendly-snippets', "onsails/lspkind.nvim", "nvim-tree/nvim-web-devicons" },
+
+    -- use a release tag to download pre-built binaries
+    version = '1.*',
+    opts = {
+      keymap = {
+        preset = 'default',
+        ['<CR>'] = { 'accept', 'fallback' },
+        ['<C-l>'] = { 'snippet_forward', 'fallback' },
+        ['<C-h>'] = { 'snippet_backward', 'fallback' },
       },
-      menu = {
-        draw = {
-          components = {
-            kind_icon = {
-              text = function(ctx)
-                local icon = ctx.kind_icon
-                -- if vim.tbl_contains({ "Path" }, ctx.source_name) then
-                --   local dev_icon, _ = require("nvim-web-devicons").get_icon(ctx.label)
-                --   if dev_icon then
-                --     icon = dev_icon
-                --   end
-                -- else
-                icon = require("lspkind").symbolic(ctx.kind, {
-                  mode = "symbol",
-                })
-                -- end
-
-                return icon .. ctx.icon_gap
-              end,
-
-              -- Optionally, use the highlight groups from nvim-web-devicons
-              -- You can also add the same function for `kind.highlight` if you want to
-              -- keep the highlight groups in sync with the icons.
-              -- highlight = function(ctx)
-              --   local hl = ctx.kind_hl
-              --   if vim.tbl_contains({ "Path" }, ctx.source_name) then
-              --     local dev_icon, dev_hl = require("nvim-web-devicons").get_icon(ctx.label)
-              --     if dev_icon then
-              --       hl = dev_hl
-              --     end
-              --   end
-              --   return hl
-              -- end,
+      appearance = {
+        nerd_font_variant = 'mono'
+      },
+      completion = {
+        documentation = {
+          auto_show = true,
+          auto_show_delay_ms = 500
+        },
+        accept = {
+          auto_brackets = { -- automatically insert brackets when accepting functions/methods
+            enabled = true,
+            semantic_token_resolution = {
+              blocked_filetypes = { "scala" },
             }
           }
-        }
-      },
-    },
-    signature = {
-      enabled = true
-    },
-    sources = {
-      default = { 'lsp', 'path', 'snippets', 'buffer' },
-    },
+        },
+        menu = {
+          draw = {
+            columns = { {"kind_icon"}, { "label", "label_description", gap = 1, "kind", "source_name" } },
+            components = {
+              kind_icon = {
+                text = function(ctx)
+                  local icon = ctx.kind_icon
+                  -- if vim.tbl_contains({ "Path" }, ctx.source_name) then
+                  --   local dev_icon, _ = require("nvim-web-devicons").get_icon(ctx.label)
+                  --   if dev_icon then
+                  --     icon = dev_icon
+                  --   end
+                  -- else
+                  icon = require("lspkind").symbolic(ctx.kind, {
+                    mode = "symbol",
+                  })
+                  -- end
 
-    fuzzy = { implementation = "prefer_rust_with_warning" }
-  },
-  opts_extend = { "sources.default" }
+                  return icon .. ctx.icon_gap
+                end,
+
+                -- Optionally, use the highlight groups from nvim-web-devicons
+                -- You can also add the same function for `kind.highlight` if you want to
+                -- keep the highlight groups in sync with the icons.
+                -- highlight = function(ctx)
+                --   local hl = ctx.kind_hl
+                --   if vim.tbl_contains({ "Path" }, ctx.source_name) then
+                --     local dev_icon, dev_hl = require("nvim-web-devicons").get_icon(ctx.label)
+                --     if dev_icon then
+                --       hl = dev_hl
+                --     end
+                --   end
+                --   return hl
+                -- end,
+              }
+            }
+          }
+        },
+      },
+      signature = {
+        enabled = true
+      },
+      sources = {
+        default = { 'lazydev', 'lsp', 'path', 'snippets', 'buffer' },
+        providers = {
+          lazydev = {
+            name = "LazyDev",
+            module = "lazydev.integrations.blink",
+            -- make lazydev completions top priority (see `:h blink.cmp`)
+            score_offset = 100,
+          },
+        },
+      },
+      fuzzy = { implementation = "prefer_rust_with_warning" }
+    },
+    opts_extend = { "sources.default" }
+  }
 }
