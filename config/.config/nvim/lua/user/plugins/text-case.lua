dc318bb0cec70b24434ba397c406bdb4159e2818
agaro1121 <agaro1121@gmail.com>
2025-05-12 19:42:13 -0400

    removes telescope from text csae

--- config/.config/nvim/lua/user/plugins/text-case.lua
+++ config/.config/nvim/lua/user/plugins/text-case.lua
@@ -1,13 +1,8 @@
 return {
   "johmsalas/text-case.nvim",
-  keys = {
-    {"ga.", ":TextCaseOpenTelescope<CR>", mode = { "n", "v" }}
-  },
   config = function()
     require("textcase").setup({})
-    require("telescope").load_extension("textcase")
 
-    -- map({ "n", "v" }, "ga.", ":TextCaseOpenTelescope<CR>", {desc = "textcase.open"})
     map({ "n", "v" }, "ga.",
       function()
         local plugin = require('textcase.plugin.plugin')
