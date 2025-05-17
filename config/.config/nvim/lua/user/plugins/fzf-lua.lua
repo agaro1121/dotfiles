d4a224397107fd00256fd0539c208e97759e2f6f
agaro1121 <agaro1121@gmail.com>
2025-05-16 22:52:56 -0400

    adds fzf keymap

--- config/.config/nvim/lua/user/plugins/fzf-lua.lua
+++ config/.config/nvim/lua/user/plugins/fzf-lua.lua
@@ -6,6 +6,7 @@ return {
     { "<leader>fg",  function() require("fzf-lua").git_files() end,     desc = "fzf-lua.git files" },
     { "<leader>lg",  function() require("fzf-lua").grep_project() end,  desc = "fzf-lua.live grep" },
     { "<leader>lgs", function() require("fzf-lua").grep_cword() end,    desc = "fzf-lua.grep string" },
+    { "<leader>lgv", function() require("fzf-lua").grep_visual() end,    desc = "fzf-lua.grep string", mode = "v" },
     { "<leader>bs",  function() require("fzf-lua").buffers() end,       desc = "fzf-lua.buffers" },
     { "<leader>sp",  function() require("fzf-lua").spell_suggest() end, desc = "fzf-lua.spell suggest" },
     { "<leader>fb",  function() require("fzf-lua").lgrep_curbuf() end,  desc = "fzf-lua.fuzzy find in current buffer" },
