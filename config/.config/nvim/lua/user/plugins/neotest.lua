76fca495fae961f0898e269fd87d8d9df5802566
agaro1121 <agaro1121@gmail.com>
2025-05-29 23:31:20 -0400

    fixes neotest-scala, prevents dap-ui from closing

--- config/.config/nvim/lua/user/plugins/neotest.lua
+++ config/.config/nvim/lua/user/plugins/neotest.lua
@@ -1,19 +1,30 @@
 return {
   "nvim-neotest/neotest",
+  -- dir = "~/git/neotest",
   dependencies = {
     "nvim-neotest/nvim-nio",
     "nvim-lua/plenary.nvim",
     "antoinemadec/FixCursorHold.nvim",
     "nvim-treesitter/nvim-treesitter",
-    "stevanmilic/neotest-scala"
+    { "agaro1121/neotest-scala"--[[ , dir = "~/git/neotest-scala" ]] },
   },
   config = function()
     require("neotest").setup({
       adapters = {
-        require("neotest-scala")
-      },
-      status = {
-        enabled = false
+        require("neotest-scala")({
+          -- Runner to use. Will use bloop by default.
+          -- Can be a function to return dynamic value.
+          -- For backwards compatibility, it also tries to read the vim-test scala config.
+          -- Possibly values bloop|sbt.
+          runner = "bloop",
+          -- Test framework to use. Will use utest by default.
+          -- Can be a function to return dynamic value.
+          -- Possibly values utest|munit|scalatest.
+          framework = "munit",
+        }),
+        status = {
+          enabled = false
+        }
       }
     })
     map("n", "<leader>us", require("neotest").summary.toggle, { desc = "neotest.summary" })
