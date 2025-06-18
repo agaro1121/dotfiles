59f6b467d9d58e8649db7ee25d6989f54809c725
agaro <anthony.garo@disneystreaming.com>
2025-06-18 10:18:12 -0400

    updates config

--- config/.config/nvim/lua/user/plugins/otree.lua
+++ config/.config/nvim/lua/user/plugins/otree.lua
@@ -1,19 +1,22 @@
 return {
-    "agaro1121/Otree.nvim",
-    lazy = false,
-    dependencies = {
-        "stevearc/oil.nvim",
-        -- { "echasnovski/mini.icons", opts = {} },
-        "nvim-tree/nvim-web-devicons",
-    },
-    config = function()
-        require("Otree").setup({
-          win_size = 40,
-          keymaps = {
-            ["<C-h>"] = "",
-            ["<C-l>"] = "",
-         },
-        })
-        map("n","<leader>e", ":Otree<cr>", {desc = "nvimtree.Toggele"})
-    end
+  "agaro1121/Otree.nvim",
+  lazy = false,
+  dependencies = {
+    "stevearc/oil.nvim",
+    -- { "echasnovski/mini.icons", opts = {} },
+    "nvim-tree/nvim-web-devicons",
+  },
+  config = function()
+    require("Otree").setup({
+      win_size = 40,
+      keymaps = {
+        ["<C-h>"] = "",
+        ["<C-l>"] = "",
+      },
+      tree = {
+        space_after_connector = "",
+      },
+    })
+    map("n", "<leader>e", ":Otree<cr>", { desc = "nvimtree.Toggele" })
+  end
 }
--- config/.config/nvim/lua/user/plugins/visual-whitespace.lua
+++ config/.config/nvim/lua/user/plugins/visual-whitespace.lua
@@ -2,5 +2,10 @@ return {
   'mcauley-penney/visual-whitespace.nvim',
   config = true,
   event = "ModeChanged *:[vV\22]",   -- optionally, lazy load on entering visual mode
-  opts = {},
+  opts = {
+    enabled = false
+  },
+  init = function()
+    vim.keymap.set({ 'n', 'v' }, "<leader>tw", require("visual-whitespace").toggle, {})
+  end
 }
