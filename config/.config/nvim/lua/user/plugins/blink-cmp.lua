0cd347663c9673838383b91d55bd3e3a7bed72f5
agaro1121 <agaro1121@gmail.com>
2025-09-06 16:49:17 -0400

    removes lspkind

--- config/.config/nvim/lua/user/plugins/blink-cmp.lua
+++ config/.config/nvim/lua/user/plugins/blink-cmp.lua
@@ -12,7 +12,7 @@ return {
   },
   {
     'saghen/blink.cmp',
-    dependencies = { 'rafamadriz/friendly-snippets', "onsails/lspkind.nvim", "nvim-tree/nvim-web-devicons" },
+    dependencies = { 'rafamadriz/friendly-snippets', "nvim-tree/nvim-web-devicons" },
 
     -- use a release tag to download pre-built binaries
     version = '1.*',
@@ -48,39 +48,7 @@ return {
         menu = {
           draw = {
             columns = { { "kind_icon" }, { "label", "label_description", gap = 1, "kind", "source_name" } },
-            components = {
-              kind_icon = {
-                text = function(ctx)
-                  local icon = ctx.kind_icon
-                  -- if vim.tbl_contains({ "Path" }, ctx.source_name) then
-                  --   local dev_icon, _ = require("nvim-web-devicons").get_icon(ctx.label)
-                  --   if dev_icon then
-                  --     icon = dev_icon
-                  --   end
-                  -- else
-                  icon = require("lspkind").symbolic(ctx.kind, {
-                    mode = "symbol",
-                  })
-                  -- end
-
-                  return icon .. ctx.icon_gap
-                end,
-
-                -- Optionally, use the highlight groups from nvim-web-devicons
-                -- You can also add the same function for `kind.highlight` if you want to
-                -- keep the highlight groups in sync with the icons.
-                -- highlight = function(ctx)
-                --   local hl = ctx.kind_hl
-                --   if vim.tbl_contains({ "Path" }, ctx.source_name) then
-                --     local dev_icon, dev_hl = require("nvim-web-devicons").get_icon(ctx.label)
-                --     if dev_icon then
-                --       hl = dev_hl
-                --     end
-                --   end
-                --   return hl
-                -- end,
-              }
-            }
+            components = {}
           }
         },
       },
--- config/.config/nvim/lua/user/plugins/outline.lua
+++ config/.config/nvim/lua/user/plugins/outline.lua
@@ -1,15 +1,8 @@
 return {
   "hedyhli/outline.nvim",
   event = "LspAttach",
-  dependencies = { "onsails/lspkind.nvim" },
   config = function()
-    require('lspkind').init({})
-
-    require("outline").setup {
-      symbols = {
-        icon_source = 'lspkind'
-      }
-    }
+    require("outline").setup{}
 
     map("n", "<leader>o", "<cmd>Outline<CR>", { desc = "outline.Toggle" })
   end,
