6ecf619666243ca67aefea6b3ea4fee2dc682124
agaro1121 <agaro1121@gmail.com>
2025-12-14 18:01:01 -0500

    adds incremental selection

---
+++ config/.config/nvim/lua/user/plugins/nvim-treesitter-modules.lua
@@ -0,0 +1,10 @@
+return {
+  'MeanderingProgrammer/treesitter-modules.nvim',
+  dependencies = { 'nvim-treesitter/nvim-treesitter' },
+  opts = {
+    fold = { enable = false },
+    highlight = { enable = false },
+    indent = { enable = false },
+    incremental_selection = { enable = true },
+  },
+}
