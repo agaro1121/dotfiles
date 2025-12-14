3d13313ae8aa4002c64dd2e1915f2cb7374d29ab
agaro1121 <agaro1121@gmail.com>
2025-12-14 17:48:02 -0500

    fixes tree mapping

--- config/.config/nvim/lua/user/plugins/nvim-tree.lua
+++ config/.config/nvim/lua/user/plugins/nvim-tree.lua
@@ -8,6 +8,38 @@ vim.opt.termguicolors = true
 local HEIGHT_RATIO = 0.8 -- You can change this
 local WIDTH_RATIO = 0.6  -- You can change this too
 
+-- optional on_attach to modify mappings
+local function my_on_attach(bufnr)
+  local api = require('nvim-tree.api')
+
+  local function opts(desc)
+    return { desc = 'nvim-tree: ' .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+  end
+
+  vim.keymap.set("n", "<C-v>", api.node.open.vertical, opts("Open: Vertical Split"))
+  vim.keymap.set("n", "<C-x>", api.node.open.horizontal, opts("Open: Horizontal Split"))
+  vim.keymap.set("n", "H", api.node.navigate.parent_close, opts("Close Directory"))
+  vim.keymap.set("n", "L", api.node.open.edit, opts("Open"))
+  vim.keymap.set("n", "<CR>", api.node.open.edit, opts("Open"))
+  vim.keymap.set("n", "J", api.node.navigate.sibling.last, opts("Last Sibling"))
+  vim.keymap.set("n", "p", api.node.open.preview, opts("Open Preview"))
+  vim.keymap.set("n", "E", api.tree.expand_all, opts("Expand All"))
+  vim.keymap.set("n", "F", api.live_filter.clear, opts("Live Filter: Clear"))
+  vim.keymap.set("n", "f", api.live_filter.start, opts("Live Filter: Start"))
+  vim.keymap.set("n", "gy", api.fs.copy.absolute_path, opts("Copy Absolute Path"))
+  vim.keymap.set("n", "ge", api.fs.copy.basename, opts("Copy Basename"))                        -- fileName, no ext
+  vim.keymap.set("n", "y", api.fs.copy.filename, opts("Copy Name"))                             -- filename
+  vim.keymap.set("n", "Y", api.fs.copy.relative_path, opts("Copy Relative Path"))
+  vim.keymap.set("n", ".", api.tree.toggle_hidden_filter, opts("Toggle Filter: Dotfiles"))
+  vim.keymap.set("n", "I", api.tree.toggle_gitignore_filter, opts("Toggle Filter: Git Ignore"))
+  vim.keymap.set("n", "P", api.node.navigate.parent, opts("Parent Directory"))
+  vim.keymap.set("n", "q", api.tree.close, opts("Close"))
+  vim.keymap.set("n", "<ESC>", api.tree.close, opts("Close"))
+  vim.keymap.set("n", "r", api.tree.reload, opts("Refresh"))
+  vim.keymap.set("n", "go", api.node.run.system, opts("Run System"))
+  vim.keymap.set("n", "C", api.tree.collapse_all, opts("Collapse All"))
+end
+
 local opts = {
   auto_reload_on_write = true,
   disable_netrw = false,
@@ -21,34 +53,7 @@ local opts = {
   reload_on_bufenter = false,
   respect_buf_cwd = false,
   -- on_attach = "default",
-  on_attach = function ()
-    local api = require("nvim-tree.api")
-
-    local function opts(desc)
-      return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
-    end
-
-    vim.keymap.set("n", "<C-v>",       api.node.open.vertical,             opts("Open: Vertical Split"))
-    vim.keymap.set("n", "<C-x>",       api.node.open.horizontal,           opts("Open: Horizontal Split"))
-    vim.keymap.set("n", "H",           api.node.navigate.parent_close,     opts("Close Directory"))
-    vim.keymap.set("n", "L",           api.node.open.edit,                 opts("Open"))
-    vim.keymap.set("n", "J",           api.node.navigate.sibling.last,     opts("Last Sibling"))
-    vim.keymap.set("n", "p",           api.node.open.preview,              opts("Open Preview"))
-    vim.keymap.set("n", "E",           api.tree.expand_all,                opts("Expand All"))
-    vim.keymap.set("n", "F",           api.live_filter.clear,              opts("Live Filter: Clear"))
-    vim.keymap.set("n", "f",           api.live_filter.start,              opts("Live Filter: Start"))
-    vim.keymap.set("n", "gy",          api.fs.copy.absolute_path,          opts("Copy Absolute Path"))
-    vim.keymap.set("n", "ge",          api.fs.copy.basename,               opts("Copy Basename")) -- fileName, no ext
-    vim.keymap.set("n", "y",           api.fs.copy.filename,               opts("Copy Name")) -- filename
-    vim.keymap.set("n", "Y",           api.fs.copy.relative_path,          opts("Copy Relative Path"))
-    vim.keymap.set("n", ".",           api.tree.toggle_hidden_filter,      opts("Toggle Filter: Dotfiles"))
-    vim.keymap.set("n", "I",           api.tree.toggle_gitignore_filter,   opts("Toggle Filter: Git Ignore"))
-    vim.keymap.set("n", "P",           api.node.navigate.parent,           opts("Parent Directory"))
-    vim.keymap.set("n", "q",           api.tree.close,                     opts("Close"))
-    vim.keymap.set("n", "r",           api.tree.reload,                    opts("Refresh"))
-    vim.keymap.set("n", "o",           api.node.run.system,                opts("Run System"))
-    vim.keymap.set("n", "C",           api.tree.collapse_all,              opts("Collapse All"))
-  end,
+  on_attach = my_on_attach,
   select_prompts = false,
   view = {
     centralize_selection = false,
@@ -70,7 +75,7 @@ local opts = {
         local window_h_int = math.floor(window_h)
         local center_x = (screen_w - window_w) / 2
         local center_y = ((vim.opt.lines:get() - window_h) / 2)
-                         - vim.opt.cmdheight:get()
+            - vim.opt.cmdheight:get()
         return {
           border = "rounded",
           relative = "editor",
@@ -79,7 +84,7 @@ local opts = {
           width = window_w_int,
           height = window_h_int,
         }
-        end,
+      end,
     },
     width = function()
       return math.floor(vim.opt.columns:get() * WIDTH_RATIO)
@@ -279,9 +284,9 @@ local opts = {
 function open_in_oil()
   local api = require("nvim-tree.api")
   local oil_status_ok, _ = pcall(require, "oil")
-    if not oil_status_ok then
-      return
-    end
+  if not oil_status_ok then
+    return
+  end
   local node = api.tree.get_node_under_cursor()
   if not node then
     vim.notify("No node under cursor", vim.log.levels.WARN)
@@ -300,14 +305,14 @@ return {
   },
   config = function()
     require("nvim-tree").setup(opts)
-    map("n","<leader>e", ":NvimTreeToggle<cr>", {desc = "nvimtree.Toggele"})
-    map("n","<leader>s", ":NvimTreeFindFile<cr>", {desc = "nvimtree.find file in tree"})
+    map("n", "<leader>e", ":NvimTreeToggle<cr>", { desc = "nvimtree.Toggele" })
+    map("n", "<leader>s", ":NvimTreeFindFile<cr>", { desc = "nvimtree.find file in tree" })
     vim.api.nvim_create_autocmd("FileType", {
       pattern = "NvimTree",
       callback = function()
         vim.keymap.set(
           "n",
-          "go",
+          "o",
           open_in_oil,
           { buffer = true, desc = "Open Oil in directory" }
         )
