5dda3ef3da6a24c0184af31a06e3aed93a7d99ba
agaro1121 <agaro1121@gmail.com>
2025-12-16 19:03:53 -0500

    treesitter updates

--- config/.config/nvim/lua/user/plugins/nvim-treesitter-modules.lua
+++ config/.config/nvim/lua/user/plugins/nvim-treesitter-modules.lua
@@ -2,7 +2,7 @@ return {
   'MeanderingProgrammer/treesitter-modules.nvim',
   dependencies = { 'nvim-treesitter/nvim-treesitter' },
   opts = {
-    auto_install = true,
+    auto_install = false,
     fold = { enable = false },
     highlight = { enable = false },
     indent = { enable = false },
