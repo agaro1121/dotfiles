06f5fb6a16c80f1c19afb63926a190583b345339
agaro <anthony.garo@disneystreaming.com>
2025-12-24 12:40:20 -0500

    uses treesitter modules

--- config/.config/nvim/lua/user/plugins/nvim-treesitter.lua
+++ config/.config/nvim/lua/user/plugins/nvim-treesitter.lua
@@ -1,77 +1,73 @@
 ---@module "lazy"
 ---@type LazySpec
 return {
-  'nvim-treesitter/nvim-treesitter',
-  dependencies = {
-    'nvim-treesitter/nvim-treesitter-context',
+  {
+    'nvim-treesitter/nvim-treesitter',
+    dependencies = {
+      'nvim-treesitter/nvim-treesitter-context',
+    },
+    lazy = false,
+    branch = 'main',
+    build = ':TSUpdate',
   },
-  lazy = false,
-  branch = 'main',
-  build = ':TSUpdate',
-  config = function()
-    local ts = require('nvim-treesitter')
-
-    -- Install core parsers at startup
-    ts.install({
-      'bash',
-      -- 'comment',
-      'css',
-      'diff',
-      'git_config',
-      'git_rebase',
-      'gitcommit',
-      'gitignore',
-      'html',
-      'javascript',
-      'json',
-      'lua',
-      'luadoc',
-      'make',
-      'markdown',
-      'markdown_inline',
-      'python',
-      'query',
-      'regex',
-      'toml',
-      'vim',
-      'vimdoc',
-      'xml',
-      'scala',
-      'yaml',
-      'latex'
-    }, {
-      max_jobs = 8,
-    })
-
-    local group = vim.api.nvim_create_augroup('TreesitterSetup', { clear = true })
-
-    local ignore_filetypes = {
-      'checkhealth',
-      'lazy',
-      'mason',
-    }
-
-    -- Auto-install parsers and enable highlighting on FileType
-    vim.api.nvim_create_autocmd('FileType', {
-      group = group,
-      desc = 'Enable treesitter highlighting and indentation',
-      callback = function(event)
-        if vim.tbl_contains(ignore_filetypes, event.match) then
-          return
-        end
-
-        local lang = vim.treesitter.language.get_lang(event.match) or event.match
-        local buf = event.buf
-
-        -- Start highlighting immediately (works if parser exists)
-        pcall(vim.treesitter.start, buf, lang)
-
-        -- Enable treesitter indentation
-        vim.bo[buf].indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
-
-        -- Install missing parsers (async, no-op if already installed)
-        ts.install({ lang })
-      end,
-    })
-  end,
+  {
+    'MeanderingProgrammer/treesitter-modules.nvim',
+    opts = {},
+    config = function()
+      require('treesitter-modules').setup({
+        ensure_installed = {
+          'bash',
+          'comment',
+          'css',
+          'diff',
+          'git_config',
+          'git_rebase',
+          'gitcommit',
+          'gitignore',
+          'html',
+          'javascript',
+          'json',
+          'lua',
+          'luadoc',
+          'make',
+          'markdown',
+          'markdown_inline',
+          'python',
+          'query',
+          'regex',
+          'toml',
+          'vim',
+          'vimdoc',
+          'xml',
+          'scala',
+          'yaml',
+          'latex'
+        },
+        ignore_install = {},
+        sync_install = false,
+        auto_install = true,
+        fold = { enable = false, disable = false },
+        highlight = {
+          enable = true,
+          -- setting this to true will run `:h syntax` and tree-sitter at the same time
+          -- set this to `true` if you depend on 'syntax' being enabled
+          -- using this option may slow down your editor, and duplicate highlights
+          -- instead of `true` it can also be a list of languages
+          additional_vim_regex_highlighting = false,
+        },
+        incremental_selection = {
+          enable = true,
+          -- set value to `false` to disable individual mapping
+          -- node_decremental captures both node_incremental and scope_incremental
+          keymaps = {
+            init_selection = 'gnn',
+            node_incremental = 'grn',
+            scope_incremental = 'grc',
+            node_decremental = 'grm',
+          },
+        },
+        indent = { enable = true },
+      })
+    end
+  }
 }
