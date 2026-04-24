4a9d6dfb02f173e7c9627ec7fcdd0cd1eb0b4831
agaro1121 <agaro1121@gmail.com>
2026-04-24 19:32:46 -0400

    Adds treesitter objects

--- config/.config/nvim/lua/user/plugins/nvim-treesitter.lua
+++ config/.config/nvim/lua/user/plugins/nvim-treesitter.lua
@@ -1,10 +1,12 @@
 vim.pack.add({
   -- 'https://github.com/nvim-treesitter/nvim-treesitter-context',
   { src = 'https://github.com/nvim-treesitter/nvim-treesitter', version = 'main' },
-  'https://github.com/MeanderingProgrammer/treesitter-modules.nvim'
+  'https://github.com/MeanderingProgrammer/treesitter-modules.nvim',
+  { src = "https://github.com/nvim-treesitter/nvim-treesitter-textobjects", version = 'main' }
 })
 
 require('nvim-treesitter').setup()
+
 require('treesitter-modules').setup({
   ensure_installed = {
     'bash',
@@ -58,3 +60,65 @@ require('treesitter-modules').setup({
   },
   indent = { enable = true },
 })
+
+
+require("nvim-treesitter-textobjects").setup {
+  select = {
+    lookahead = false,
+    include_surrounding_whitespace = false,
+  },
+}
+
+-- Select keyvim.keymap.sets
+-- You can use the capture groups defined in `textobjects.scm`
+vim.keymap.set({ "x", "o" }, "af", function()
+  require "nvim-treesitter-textobjects.select".select_textobject("@function.outer", "textobjects")
+end)
+vim.keymap.set({ "x", "o" }, "if", function()
+  require "nvim-treesitter-textobjects.select".select_textobject("@function.inner", "textobjects")
+end)
+vim.keymap.set({ "x", "o" }, "ac", function()
+  require "nvim-treesitter-textobjects.select".select_textobject("@class.outer", "textobjects")
+end)
+vim.keymap.set({ "x", "o" }, "ic", function()
+  require "nvim-treesitter-textobjects.select".select_textobject("@class.inner", "textobjects")
+end)
+vim.keymap.set({ "x", "o" }, "ap", function()
+  require "nvim-treesitter-textobjects.select".select_textobject("@parameter.outer", "textobjects")
+end)
+
+-- Swap keyvim.keymap.sets
+vim.keymap.set("n", "<leader>a", function()
+  require("nvim-treesitter-textobjects.swap").swap_next "@parameter.inner"
+end)
+vim.keymap.set("n", "<leader>A", function()
+  require("nvim-treesitter-textobjects.swap").swap_previous "@parameter.outer"
+end)
+
+-- keyvim.keymap.sets
+-- You can use the capture groups defined in `textobjects.scm`
+vim.keymap.set({ "n", "x", "o" }, "]f", function()
+  require("nvim-treesitter-textobjects.move").goto_next_start("@function.outer", "textobjects")
+end)
+vim.keymap.set({ "n", "x", "o" }, "]c", function()
+  require("nvim-treesitter-textobjects.move").goto_next_start("@class.outer", "textobjects")
+end)
+vim.keymap.set({ "n", "x", "o" }, "[f", function()
+  require("nvim-treesitter-textobjects.move").goto_previous_start("@function.outer", "textobjects")
+end)
+vim.keymap.set({ "n", "x", "o" }, "[c", function()
+  require("nvim-treesitter-textobjects.move").goto_previous_start("@class.outer", "textobjects")
+end)
+
+vim.keymap.set({ "n", "x", "o" }, "]F", function()
+  require("nvim-treesitter-textobjects.move").goto_next_end("@function.outer", "textobjects")
+end)
+vim.keymap.set({ "n", "x", "o" }, "]C", function()
+  require("nvim-treesitter-textobjects.move").goto_next_end("@class.outer", "textobjects")
+end)
+vim.keymap.set({ "n", "x", "o" }, "[F", function()
+  require("nvim-treesitter-textobjects.move").goto_previous_end("@function.outer", "textobjects")
+end)
+vim.keymap.set({ "n", "x", "o" }, "[C", function()
+  require("nvim-treesitter-textobjects.move").goto_previous_end("@class.outer", "textobjects")
+end)
