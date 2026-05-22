11203cdd75a689606f70f63d7bcf0e5c735304d4
agaro1121 <agaro1121@gmail.com>
2026-05-21 22:11:32 -0400

    changes default live grep impl

--- config/.config/nvim/lua/user/plugins/fzf.lua
+++ config/.config/nvim/lua/user/plugins/fzf.lua
@@ -14,7 +14,8 @@ fzflua.register_ui_select() -- use fzf-lua as neovim's `vim.ui.select`
 
 vim.keymap.set('n', '<leader>ff', fzflua.files, { desc = 'fzflua: Find files' })
 vim.keymap.set('n', '<leader>tk', fzflua.keymaps, { desc = 'fzflua: Keymaps' })
-vim.keymap.set('n', '<leader>lg', fzflua.live_grep_native, { desc = 'fzflua: Live grep' })
+-- vim.keymap.set('n', '<leader>lg', fzflua.live_grep_native, { desc = 'fzflua: Live grep' })
+vim.keymap.set('n', '<leader>lg', fzflua.live_grep, { desc = 'fzflua: Live grep glob' })
 vim.keymap.set('v', '<leader>lgs', fzflua.grep_visual, { desc = 'fzflua: Live grep highlighted text' })
 vim.keymap.set('n', '<leader>lgs', fzflua.grep_cword, { desc = 'fzflua: Live grep word under cursor' })
 vim.keymap.set('n', '<leader>/',  fzflua.lgrep_curbuf, { desc = 'fzflua: Live grep current file' })
